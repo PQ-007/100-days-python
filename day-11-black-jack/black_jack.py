7d20aa3931bbc5c9b80c716863e8cdfbbdda42ff
bilguuntushig <bilguuntushig.a@gmail.com>
2026-06-09 17:17:50 +0800

    Refactor Blackjack game logic to improve card dealing, scoring, and game flow

--- day-11-black-jack/black_jack.py
+++ day-11-black-jack/black_jack.py
@@ -1,5 +1,39 @@
 import random
-print(r'''
+
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
+    return random.choice(cards)
+
+
+def calculate_score(hand):
+    if sum(hand) == 21 and len(hand) == 2:
+        return 0
+    if 11 in hand and sum(hand) > 21:
+        hand.remove(11)
+        hand.append(1)
+    return sum(hand)
+
+def compare(player_score, computer_score):
+    if player_score == computer_score:
+        return "Draw 🙃"
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack 😱"
+    elif player_score == 0:
+        return "Win with a Blackjack 😎"
+    elif player_score > 21:
+        return "You went over. You lose 😭"
+    elif computer_score > 21:
+        return "Opponent went over. You win 😁"
+    elif player_score > computer_score:
+        return "You win 😃"
+    else:
+        return "You lose 😤" 
+    
+
+
+def play_game():       
+    print(r'''
 .------.            _     _            _    _            _    
 |A_  _ |.          | |   | |          | |  (_)          | |   
 |( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
@@ -8,57 +42,41 @@ print(r'''
 '-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\
       |  \/ K|                            _/ |                
       '------'                           |__/   
-''')
+''')  
+    running = True
+    player_hand = []
+    computer_hand = []
+    computer_score = -1
+    player_score = -1
 
-def deal_card():
-    return random.randint(1, 11)
-
-
-def calculate_score(hand):
-    return sum(hand)
-
-
-running = True
-while running:
-    print("Do you want to play a game of Blackjack? Type 'y' or 'n'")
-    if input().lower() == 'y':
-        player_hand = [deal_card(), deal_card()]
-        computer_hand = [deal_card(), deal_card()]
-
-        print(f"Your cards: {player_hand}, current score: {calculate_score(player_hand)}")
-        print(f"Computer's first card: {computer_hand[0]}")
-        player_score = calculate_score(player_hand)
-        while player_score < 21:
-            print("Type 'y' to get another card, type 'n' to pass.")
-            if input().lower() == 'y':
-                player_hand.append(deal_card())
-                player_score = calculate_score(player_hand)
-                print(f"Your cards: {player_hand}, current score: {player_score}")
-                if player_score > 21:
-                    break
-            else:
-                break
-
-        computer_score = calculate_score(computer_hand)
-        while player_score <= 21 and computer_score < 17:
+    for _ in range(2):
+            player_hand.append(deal_card())
             computer_hand.append(deal_card())
-            computer_score = calculate_score(computer_hand)
 
+    while running:
+        player_score = calculate_score(player_hand)
         computer_score = calculate_score(computer_hand)
 
-        print(f"Your final hand: {player_hand}, final score: {player_score}")
-        print(f"Computer's final hand: {computer_hand}, final score: {computer_score}")
+        print(f"Your cards: {player_hand}, current score: {player_score}")
+        print(f"Computer's first card: {computer_hand[0]}")
 
-        if player_score > 21:
-            print("You went over. You lose 😭")
-        elif computer_score > 21:
-            print("Computer went over. You win 😁")
-        elif player_score > computer_score:
-            print("You win 😁")
-        elif player_score < computer_score:
-            print("You lose 😭")
-        else:
-            print("It's a draw 🙃")
-    else:
-        print("Exiting the game. Goodbye!")
-        running = False
+        if player_score == 0 or computer_score == 0 or player_score > 21:
+            running = False
+        else: 
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+            if user_should_deal == "y":
+                player_hand.append(deal_card())
+            else:
+                running = False
+
+    while computer_score != 0 and computer_score < 17:
+        computer_hand.append(deal_card())
+        computer_score = calculate_score(computer_hand)
+        
+    print(f"Your final hand: {player_hand}, final score: {player_score}")
+    print(f"Computer's final hand: {computer_hand}, final score: {computer_score}")
+    print(compare(player_score, computer_score))
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    print("\033c", end="")
+    play_game()
