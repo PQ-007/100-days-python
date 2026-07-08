6fa384441ef7761a05cba8e637d8b4ba76aa8b11
bilguuntushig <bilguuntushig.a@gmail.com>
2026-07-08 23:49:07 +0800

    Add Hirst painting project with turtle graphics
    - Implemented a grid of colored dots using turtle graphics
    - Utilized a predefined color palette for the dots
    - Set turtle speed to fastest for efficient drawing

--- day-18-turtle-gui/main.py
+++ day-18-turtle-gui/main.py
@@ -1,30 +1,49 @@
-from turtle import Turtle, Screen
-
-timmy = Turtle()
+import turtle as t
+from random import choice
+timmy = t.Turtle()
 timmy.shape("turtle")
 timmy.color("red")
 
-for _ in range(4):
-    timmy.forward(100)
-    timmy.right(90)
+# for _ in range(4):
+#     timmy.forward(100)
+#     timmy.right(90)
 
-for _ in range(4):
-    timmy.forward(10)
-    timmy.penup()
-    timmy.forward(10)
-    timmy.pendown()
+# for _ in range(4):
+#     timmy.forward(10)
+#     timmy.penup()
+#     timmy.forward(10)
+#     timmy.pendown()
 
-for i in range(8):
-    for j in range(i+3):
-        timmy.forward(100)
-        timmy.right(360/(i+3))
+# for i in range(8):
+#     for j in range(i+3):
+#         timmy.forward(100)
+#         timmy.right(360/(i+3))
+
+t.colormode(255)
+
+def random_color():
+    r = choice(range(256))
+    g = choice(range(256))
+    b = choice(range(256))
+    return (r, g, b)
+
+# turn = [0, 90, 180, 270]
+# timmy.pensize(10)
+# for _ in range(200):
+#     timmy.color(random_color())
+#     timmy.forward(30)
+#     timmy.setheading(choice(turn))
+
+timmy.speed("fastest")
+
+for _ in range(100):
+    timmy.color(random_color())
+    timmy.circle(100)
+    timmy.right(5)
 
 
 
 
 
-
-
-
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
